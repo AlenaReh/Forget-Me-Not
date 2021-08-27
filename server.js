c08250a49269f8f710f5323bd5758afcc2f7962e
Alena Rehberger <alenarehberger92@gmail.com>
2021-08-26 21:29:48 -0500

    Added routes js files and required them in the server.js

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
 //GET Routes
+const apiroutes = require('./routes/APIroutes')(app);
+const htmlroutes = require('./routes/HTMLroutes')(app);
 
 //We are listening
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
