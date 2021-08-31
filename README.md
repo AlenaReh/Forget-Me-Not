af2c52dfbb9df4407b78f3a44de0ae0a6f111d1f
Alena Rehberger <alenarehberger92@gmail.com>
2021-08-31 16:16:38 -0500

    Added a README file with demo picture, cleaned up the code from consoles

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Forget-Me-Not
+
+https://github.com/AlenaReh/Forget-Me-Not
+https://forgetmenot-notetaker.herokuapp.com/notes
+
+## Description 
+
 This application can be used to write and save notes.
+  
+## Table of Contents
+
+  - [Usage](#Usage)
+  - [Demo](#Demo)
+  - [Contributions](#Contributions)
+  - [Credits](#Credits)
+  - [Contacts](#Contacts)
+  
+## Usage
+
+A simple application to stay on top of your daily tasks. It uses an Express.js back end and can save and retrieve note data from a JSON file.
+
+## Demo 
+
+  * ![demo_still](public/assets/images/demo.png)
+
+## Contributions
+Alena Rehberger
+
+## Credits
+Thanks to w3schools https://w3schools.com and Trilogy Education Services for providing all of the necessery tools for building this application.
+
+## Contacts
+If you have any questions, feel free to cantact me at: 
+  * GitHub: https://github.com/AlenaReh
+  * Linkedin: https://www.linkedin.com/in/alena-rehberger/
+  * E-mail: alenarehberger92@gmail.com
--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -6,14 +6,14 @@ const fs = require("fs");
 
 module.exports = function (app) {
   app.get("/api/notes", (req, res) => {
-    //Reading datafro mJSON file using Fs module readFile()
+    //Reading data from JSON file using Fs module readFile()
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) {
         console.error(err);
       } else {
-        //parsing data in JSON object to display on the HTML , in case of no values assign a blank array
+        //Parsing data in JSON object to display on the HTML, in case of no values - assign a blank array
         let storeNotes = JSON.parse(data) || [];
-        //returning the parsed object to the HTML page
+        //Returning the parsed object to the HTML page
         res.json(storeNotes);
       }
     });
@@ -26,43 +26,36 @@ module.exports = function (app) {
         console.error(err);
       } else {
         let storeNotes = JSON.parse(data) || [];
-
-        // console.log("New Note", req.body);
         storeNotes.push(req.body);
-        // console.log("Combined Notes", storeNotes);
-        //updating the JSON file with new combined Notes
+        //Updating the JSON file with new combined Notes
         writeToFile("./db/db.json", storeNotes, (err) => {
           if (err) throw err;
         });
-        //return JSON object to the display HTML.
+        //Return JSON object to the display HTML.
         res.json(storeNotes);
       }
     });
   });
 
-  //to delete notes
+  //To delete notes
   app.delete("/api/notes/:id", (req, res) => {
-    //converting params into an integer
+    //Converting params into an integer
     let deletedNote = parseInt(req.params.id);
     let newStoredNotes = [];
-
     fs.readFile("./db/db.json", "utf8", (err, data) => {
-      //Refresh values
+      //Refreshing values
       storeNotes = [].concat(JSON.parse(data));
-
       for (let i = 0; i < storeNotes.length; i++) {
         //Filtering out the delted note from the Note  list
         if (deletedNote !== storeNotes[i].id) {
           newStoredNotes.push(storeNotes[i]);
         }
       }
-      //   console.log("Delete", newStoredNotes);
       //Updating the JSON file with the filtered Data
       writeToFile("./db/db.json", newStoredNotes, (err) => {
         if (err) throw err;
       });
-      //   console.log("Deleted successfully!!", deletedNote);
-      //returning the updated JSON object tot hTML page
+      //Returning the updated JSON object to hTML page
       res.json(newStoredNotes);
     });
   });
