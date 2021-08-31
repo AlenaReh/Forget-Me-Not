65b5ee4483a582ca6e07f17bb71184c5679c88a7
Alena Rehberger <alenarehberger92@gmail.com>
2021-08-31 15:36:57 -0500

    Notes are deleting now

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,60 +1,69 @@
-const {readAndAppend, readFromFile, readingData, writeToFile} = require("../helpers/fsUtils");
-let storeNotes;
+const {
+  writeToFile,
+} = require("../helpers/fsUtils");
+
 const fs = require("fs");
-const { ADDRGETNETWORKPARAMS } = require("dns");
 
 module.exports = function (app) {
-  app.get("/api/notes", function (req, res) {
-    console.log("reading data .. ", "./db/db.json");
-    // const allNotes = async() =>{
-    //     storeNotes = await readFile("./db/db.json");
-    //     return JSON.parse(storeNotes);
-
-    // console.log("readFromFile", storeNotes);
-    // res.json(storeNotes)
-
+  app.get("/api/notes", (req, res) => {
+    //Reading datafro mJSON file using Fs module readFile()
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) {
         console.error(err);
       } else {
-        storeNotes = JSON.parse(data);
-        console.log("parsedData", storeNotes);
+        //parsing data in JSON object to display on the HTML , in case of no values assign a blank array
+        let storeNotes = JSON.parse(data) || [];
+        //returning the parsed object to the HTML page
         res.json(storeNotes);
       }
     });
   });
 
-  app.post("/api/notes", function (req, res) {
-    // console.log("New Note", req.body);
-    storeNotes.push(req.body);
-    // console.log("Combined Notes", storeNotes);
+  app.post("/api/notes", (req, res) => {
+    //Reading data from JSON file using Fs module readFile()
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        let storeNotes = JSON.parse(data) || [];
 
-    writeToFile("./db/db.json", storeNotes, err => {
-        if(err) 
-        throw(err);
+        // console.log("New Note", req.body);
+        storeNotes.push(req.body);
+        // console.log("Combined Notes", storeNotes);
+        //updating the JSON file with new combined Notes
+        writeToFile("./db/db.json", storeNotes, (err) => {
+          if (err) throw err;
+        });
+        //return JSON object to the display HTML.
+        res.json(storeNotes);
+      }
     });
-    res.json(true);
   });
 
-    //to delete notes
-    app.delete('/api/notes/:id', (req, res) => {
-        const deletedNote = parseInt(req.params.id);
-        let newStoreNotes = [];
-        fs.readFile('./db/db.json', (data) => {
-            storeNotes = JSON.parse(data);
-            for (let i=0; i<storeNotes.length; i++){
-                if(deletedNote !== storeNotes[i].id){
-                    newStoreNotes.push(storeNotes[i])
-                }
-            }
-            return newStoreNotes
-        }).then(function(storeNotes){
-            fs.writeToFile('./db/db.json', JSON.stringify(storeNotes))
-            console.log('Sucsess!')
-        });
+  //to delete notes
+  app.delete("/api/notes/:id", (req, res) => {
+    //converting params into an integer
+    let deletedNote = parseInt(req.params.id);
+    let newStoredNotes = [];
 
-    })
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      //Refresh values
+      storeNotes = [].concat(JSON.parse(data));
 
-
+      for (let i = 0; i < storeNotes.length; i++) {
+        //Filtering out the delted note from the Note  list
+        if (deletedNote !== storeNotes[i].id) {
+          newStoredNotes.push(storeNotes[i]);
+        }
+      }
+      //   console.log("Delete", newStoredNotes);
+      //Updating the JSON file with the filtered Data
+      writeToFile("./db/db.json", newStoredNotes, (err) => {
+        if (err) throw err;
+      });
+      //   console.log("Deleted successfully!!", deletedNote);
+      //returning the updated JSON object tot hTML page
+      res.json(newStoredNotes);
+    });
+  });
 };
-
